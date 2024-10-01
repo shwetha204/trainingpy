a478b5884d6e916720548bfce2801f075c1fd170
shwetha204 <you@example.com>
2024-10-01 16:46:04 +0530

    added

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
 print("hello world")
+
+#Simple Alarm Clock Description: Develop a command-line alarm clock that lets users set a specific time for an alarm.
+#Develop a number guessing game where the computer randomly selects a number, and the player has to guess it.
+#Build a simple command-line calculator that can perform basic operations such as addition, subtraction, multiplication, and division.
+#Create a command-line application to track daily expenses.
+#Create a command-line quiz application that asks users multiple-choice questions and provides feedback on their answers.
